abd2bf664e253e20d3916d69bdd8134e51d786fb
“Carlos <crocha2@cuc.edu.co>
2017-05-23 20:58:16 -0500

    Spinner sin terminar

--- app/src/main/java/com/example/user/proyectofinal/Ingreso_Producto.java
+++ app/src/main/java/com/example/user/proyectofinal/Ingreso_Producto.java
@@ -3,7 +3,9 @@ package com.example.user.proyectofinal;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 public class Ingreso_Producto extends AppCompatActivity {
@@ -12,15 +14,25 @@ public class Ingreso_Producto extends AppCompatActivity {
     private EditText cajaSerie;
     private EditText cajaDescripcion;
     private EditText cajaCliente;
+    private Spinner comboModelo;
+    private ArrayAdapter<String> adapter;
+    private String[] opc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ingreso__producto);
+
         cajaModelo = (EditText)findViewById(R.id.txtModeloPro);
         cajaSerie = (EditText)findViewById(R.id.txtSeriePro);
         cajaDescripcion = (EditText)findViewById(R.id.txtDescripcion);
         cajaCliente = (EditText)findViewById(R.id.txtClientePro);
+
+        comboModelo=(Spinner)findViewById(R.id.cmbModelo);
+        opc=this.getResources().getStringArray(R.array.modelos);
+        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,opc);
+
+        comboModelo.setAdapter(adapter);
     }
 
     public void guardarPro(View v){
@@ -30,11 +42,11 @@ public class Ingreso_Producto extends AppCompatActivity {
 
         if(validarTodo()){
             serie = cajaSerie.getText().toString();
-            modelo = cajaModelo.getText().toString();
+            modelo = comboModelo.getSelectedItem().toString();
             descripcion = cajaDescripcion.getText().toString();
             cliente = cajaCliente.getText().toString();
-
-            foto = String.valueOf(numeroFoto(modelo));
+            foto=
+            //foto = String.valueOf(numeroFoto(modelo));
 
             p = new Producto(foto, serie, modelo, descripcion, cliente);
             p.guardarPro(getApplicationContext());
@@ -46,37 +58,38 @@ public class Ingreso_Producto extends AppCompatActivity {
         }
     }
 
-    public int numeroFoto(String modelo){
+    /*public int numeroFoto(String modelo){
         int numero, aux = 0;
 
-        int fotos[] = {R.drawable.ups1000,R.drawable.ups1300,R.drawable.ups1500,R.drawable.upsbr24,R.drawable.upsbge,R.drawable.upsbe};
+        int fotos[] = {R.drawable.ups1000,R.drawable.ups1300,R.drawable.ups1500,R.drawable.br24bpg,R.drawable.bge50ml,R.drawable.be350g_lm};
 
-            if (modelo == "ups1000"){
+            if (modelo.equals(R.drawable.ups1000)){
                 numero = fotos[0];
                 aux = numero;
 
-            }if (modelo == "ups1300"){
+            }if (modelo.equals(R.drawable.ups1300)){
                 numero = fotos[1];
                 aux = numero;
 
-            }if (modelo == "ups1500"){
+            }if (modelo.equals(R.drawable.ups1500)){
                 numero = fotos[2];
                 aux = numero;
 
-            }if (modelo == "br24bpg"){
+            }if (modelo.equals(R.drawable.br24bpg)){
                 numero = fotos[3];
                 aux = numero;
 
-            }if (modelo == "bge50ml"){
+            }if (modelo.equals(R.drawable.bge50ml)){
                 numero = fotos[4];
                 aux = numero;
 
-            }if (modelo == "be350g-lm"){
+            }if (modelo.equals(R.drawable.be350g_lm)){
                 numero = fotos[5];
                 aux = numero;
             }
+
         return fotos[aux];
-    }
+    }*/
 
 
     public boolean validarTodo(){
